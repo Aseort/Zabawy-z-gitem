c6278d3171cd609ce259e0abf5332245086db4d1
Aseort <pogoda.kotwica@gmail.com>
2019-11-19 23:41:30 +0100

    some help

--- js.js
+++ js.js
@@ -1,90 +1,119 @@
 
-var enterButton = document.getElementById("enter");
-var input = document.getElementById("userInput");
-var ul = document.querySelector("ul");
-var item = document.getElementsByTagName("li");
-var deleteBtn = document.getElementById("delete1");
-var sticker = document.querySelector(".sticker");
-var btnAdd = document.getElementById("plus");
-var addItem = document.querySelector(".add-items");
-var body = document.querySelector("body");
-var btnDel1 = document.getElementById("enter1");
-
-function inputLength(){
-	return input.value.length;
-} 
-
-function createListElement() {
-	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
-	ul.appendChild(li); 
-	input.value = ""; 
+const body = document.querySelector('body');
+const buttonAddSticker = document.getElementById('plus');
+ 
+let stickersIds = 1;
+ 
+buttonAddSticker.addEventListener('click', addNewSticker);
+ 
+function addNewSticker(){
+ 
+    const id = stickersIds;
+     
+    const randomStickerCoord = Math.floor(Math.random()*500);
+     
+    const mainSticker = document.createElement('div');
+    mainSticker.id = 'sticker'+id;
+    mainSticker.classList.add('sticker');
+    mainSticker.style.display='block';
+    mainSticker.style.top =`${randomStickerCoord}px`;
+    mainSticker.style.left =`${randomStickerCoord}px`;
+ 
+    const headerSticker = document.createElement('div');
+    headerSticker.classList.add('bar');
+    headerSticker.addEventListener('mousedown',startMove);
+ 
+    const delStickerButton = document.createElement('button');
+    delStickerButton.onclick = function() {deleteSticker(id);};
+    delStickerButton.innerText = 'X';
+ 
+    const taskList = document.createElement('ul');
+    taskList.id = 'task-list'+id;
+ 
+    const addTaskForm = document.createElement('form');
+    addTaskForm.classList.add('add-items');
+ 
+    const taskInput = document.createElement('input');
+    taskInput.setAttribute('type', 'text');
+    taskInput.setAttribute('placeholder', 'treść notatki');
+    taskInput.id = 'task-text'+id;
+ 
+    const buttonsPanel = document.createElement('div');
+    buttonsPanel.classList.add('save');
+ 
+    const addTaskButton = document.createElement('button');
+    addTaskButton.innerText = 'Save';
+    addTaskButton.onclick = function(e) {
+        e.preventDefault();
+        addTask(id);
+    };
+ 
+    const delTasksButton = document.createElement('button');
+    delTasksButton.id = 'enter1';
+    delTasksButton.innerText='Delete';
+    delTasksButton.onclick = function(e) {
+        e.preventDefault();
+        deleteAllTasks(id);
+    };
+ 
+    headerSticker.appendChild(delStickerButton);
+ 
+    buttonsPanel.appendChild(addTaskButton);
+    buttonsPanel.appendChild(delTasksButton);
+ 
+    addTaskForm.appendChild(taskInput);
+    addTaskForm.appendChild(buttonsPanel);
+ 
+    mainSticker.appendChild(headerSticker);
+    mainSticker.appendChild(taskList);
+    mainSticker.appendChild(addTaskForm);
+ 
+    body.appendChild(mainSticker);
+ 
+    stickersIds++;
 }
-function addListAfterClick(){
-	if (inputLength() > 0) { 
-		createListElement();
-	}
+ 
+function deleteSticker(id){
+    const deletedSticker = document.getElementById('sticker'+id);
+    body.removeChild(deletedSticker);
+}
+ 
+function addTask(id){
+    const taskList = document.getElementById('task-list'+id);
+    const taskText = document.getElementById('task-text'+id).value;
+ 
+    const newTask = document.createElement('li');
+    newTask.innerHTML = taskText;
+     
+    taskList.appendChild(newTask);
+    document.getElementById('task-text'+id).value = "";
+}
+ 
+function deleteAllTasks(id) {
+    const taskList = document.getElementById('task-list'+id);
+    while (taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild);
+      }
+}
+ 
+function startMove(e) {
+    const movedSticker = e.target.parentNode;
+    movedSticker.addEventListener('mousemove', move);
+}
+ 
+function move(e) {
+    const movedSticker = e.target.parentNode;
+    movedSticker.style.position = 'absolute';
+    movedSticker.style.top = e.clientY + 'px';
+    movedSticker.style.left = e.clientX + 'px';
+}
+ 
+window.addEventListener('mouseup',endMove);
+ 
+function endMove() {
+    const allStickers = document.querySelectorAll('.sticker');
+    for(let i=0;i<allStickers.length;i++){
+        allStickers[0].removeEventListener('mousemove', move);
+    }
 }
 
-function addNewBtn(){
-	
-	var random = Math.floor(Math.random()*500);
-	var new1 = document.createElement("div");
-	new1.classList.add("sticker");
-	new1.style.display="block";
-	new1.style.top =`${random}px`;
-	new1.style.left =`${random}px`;
-	var new2 = document.createElement("div");
-	new2.classList.add("bar");
-	var new3 = document.createElement("button");
-	new3.id = "delete1";
-	new3.innerText = "X";
-	var new4 = document.createElement("form");
-	new4.classList.add("add-items");
-	var new5 = document.createElement("input");
-	new5.setAttribute("type", "text");
-	new5.setAttribute("placeholder", "treść notatki");
-	new5.id = "userInput";
-	var new6 = document.createElement("ul");
-	var new7 = document.createElement("div");
-	new7.classList.add("save");
-	var new8 = document.createElement("button");
-	new8.id = "enter";
-	new8.innerText = "Save";
-	var new9 = document.createElement("button");
-	new9.id = "enter1";
-	new9.setAttribute("type","submit");
-	new9.innerText="Delete";
-	
-	new7.appendChild(new8);
-	new7.appendChild(new9);
-	new4.appendChild(new5);
-	new2.appendChild(new3);
-	new2.appendChild(new4);
-	new2.appendChild(new6);
-	new2.appendChild(new7);
-	new1.appendChild(new2);
-	body.appendChild(new1);
-
-	if (true == true)/*takie małe trolowanie XD */{
-		sticker.style.display="block";
-		sticker.style.top = `${random}px`;
-		sticker.style.left = `${random}px`;
-	}
-	
-}
-function deleteFun(){
-	sticker.style.display = "none";
-}
-function submit(e){
-	e.preventDefault();
-}
-
-function clearAll(){
-		item.classList.add("delete")
-	}
-btnDel1.addEventListener("click", clearAll);
-addItem.addEventListener("submit", submit)
-enterButton.addEventListener("click",addListAfterClick);
-deleteBtn.addEventListener("click", deleteFun);
-btnAdd.addEventListener("click", addNewBtn);
